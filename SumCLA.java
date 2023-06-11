fa5f27b1251a651d12bcc52790bcb8667cb1e591
Deepesh <73437033+Deepesh-create@users.noreply.github.com>
2023-06-11 10:42:29 +0530

    1st commit
    Program to find the sum of any number of integers entered as command line arguments in java

---
+++ SumCLA.java
@@ -0,0 +1,11 @@
+class SumCLA{
+    public static void main(String args[]){
+        int sum = 0;
+        System.out.println("Calculating Sum Of Following Number :");
+        for (int i = 0; i < args.length; i++){
+            sum = sum + Integer.parseInt(args[i]);
+            System.out.println(args[i]);
+        }
+        System.out.println("Sum = " + sum);
+    }
+}
