0dacf3dc1dffc5bf683d3e97c31ef69a0f6cc835
Deepesh <73437033+Deepesh-create@users.noreply.github.com>
2023-06-21 23:07:52 +0530

    2nd commit

--- Factorial.java
+++ Factorial.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-class Factorial{
+class Factorial {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a Number to find Factorial : ");
