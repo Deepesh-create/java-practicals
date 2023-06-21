17aadb8ec740e351c1fa00ed3e8d9de5a28c50e0
Deepesh <73437033+Deepesh-create@users.noreply.github.com>
2023-06-21 22:59:41 +0530

    Create Check_Prime.java
    Write a program in java to check if a number is prime or not, by taking the number as input from the keyboard

---
+++ Check_Prime.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class CheckPrime {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter A Number :");
+        int num = sc.nextInt();
+        int check = 0;
+        for (int i = 1; i <= num; i++) {
+            if (num % i == 0) {
+                check++;
+            }
+        }
+        if (check == 2) {
+            System.out.println("It is prime ");
+        } else {
+            System.out.println("It is not prime");
+        }
+
+    }
+}
