dd21a919a50f431b9e29285f6f3892cd86171b33
Deepesh <73437033+Deepesh-create@users.noreply.github.com>
2023-06-21 23:05:16 +0530

    3rd commit

--- Dec_To_Bin.java
+++ Dec_To_Bin.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class DecToBin {
+class DecToBin {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter Decimal Number :");
