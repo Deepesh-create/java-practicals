f7b3c5323e233dec360ad81f52bcecacf86059f0
Deepesh <73437033+Deepesh-create@users.noreply.github.com>
2023-06-21 23:06:34 +0530

    5th commit

--- SumCommand.java
+++ SumCommand.java
@@ -1,8 +1,8 @@
-class SumCommand {
-    public static void main(String args[]) {
+class SumCommand{
+    public static void main(String args[]){
         int sum = 0;
         System.out.println("Calculating Sum Of Following Number :");
-        for (int i = 0; i < args.length; i++) {
+        for (int i = 0; i < args.length; i++){
             sum = sum + Integer.parseInt(args[i]);
             System.out.println(args[i]);
         }
